a3c65d88d6d9137e75ffe68249083ab89858e450
Carlos Camacho <ccamaccl@emeal.nttdata.com>
2025-05-08 14:45:13 -0500

    add slash

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-
+import home from '../styles/home.module.css'
 export const HomePage = () => {
   return (
-    <div>HomePage V0.0.1 by Andres Camacho</div>
+    <div className={home.home_container}>HomePage V0.0.1 by Andres Camacho</div>
   )
 }
