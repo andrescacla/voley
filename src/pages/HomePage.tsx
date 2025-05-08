44649e35049b2580099e467261db7bf9d99e4c28
Carlos Camacho <ccamaccl@emeal.nttdata.com>
2025-05-08 14:56:33 -0500

    add button to start

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from 'react-router'
 import home from '../styles/home.module.css'
 export const HomePage = () => {
+  const navigate = useNavigate()
+  const onStart = () => {
+    navigate('/register')
+  }
+
   return (
-    <div className={home.home_container}>HomePage V0.0.1 by Andres Camacho</div>
+    <div className={home.home_container}>
+      HomePage V0.0.1 by Andres Camacho
+      <button className={home.button_start} onClick={onStart}>
+        Comenzar
+      </button>
+    </div>
   )
 }
