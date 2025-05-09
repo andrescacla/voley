1712c7674c05a7d20c589d3dfa13cc8fd6ddcc45
Carlos Camacho <ccamaccl@emeal.nttdata.com>
2025-05-09 15:12:30 -0500

    add validation manual with formik

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,10 @@
 import register from '../styles/register.module.css'
-import { useForm } from '../hooks/useForm'
+// import { useForm } from '../hooks/useForm'
 import { useDataContext } from '../hooks/useData'
 import { InitialForm } from '../interface/player.interface'
 import { useNavigate } from 'react-router'
 import { useTranslation } from 'react-i18next'
+import { FormikErrors, useFormik } from 'formik'
 
 export interface PlayerActions {
     id: number,
@@ -12,20 +13,54 @@ export interface PlayerActions {
 export const RegisterPage = () => {
     const { t } = useTranslation('', { keyPrefix: 'register' })
     const navigate = useNavigate()
-    const { onInputChange, formState } = useForm<InitialForm>({ player: '', number: '', position: '' })
-    const { player, number, position } = formState
     const { data, setPlayer } = useDataContext()
-    const { players } = data
-    const onSavePlayer = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        setPlayer({
-            id: Date.now(),
-            name: player,
-            statistics: [],
-            number,
-            position
-        })
+
+    const validate = ({number, player, position}: InitialForm) => {
+        const errors: FormikErrors<InitialForm> = {}
+        if (!player) {
+            errors.player = t('inputError')
+        }else if (player.length < 3) {
+            errors.player = t('minError')
+        }else if (player.length > 40) {
+            errors.player = t('maxError')
+        }
+        if (!number) {
+            errors.number = t('inputError')
+        }else if (number.length > 0 && number.length > 2) {
+            errors.number = t('numberError')
+        }
+        if (!position) {
+            errors.position = t('inputError')
+        }
+        console.log(errors)
+        return errors
     }
+    const { values, errors, touched, handleChange, handleSubmit, handleBlur } = useFormik({
+        initialValues: {
+            player: '',
+            number: '',
+            position: ''
+        },
+        onSubmit: (values) => {
+            console.log(values)
+            setPlayer({
+                id: Date.now(),
+                name: player,
+                statistics: [],
+                number,
+                position
+            })
+        },
+        validate
+    })
+    // const { onInputChange, formState } = useForm<InitialForm>({ player: '', number: '', position: '' })
+    const { player, number, position } = values
+    const { players } = data
+    
+    // const onSavePlayer = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault()
+
+    // }
 
     const onContinue = () => {
         navigate('/collects-statistics')
@@ -36,9 +71,9 @@ export const RegisterPage = () => {
         <>
             <h1 className={register.title}>{t('title')} </h1>
             <section className={register.section_register}>
-                <form onSubmit={onSavePlayer} className={register.form_register}>
+                <form noValidate onSubmit={handleSubmit} className={register.form_register}>
                     <div className={register.form_control}>
-                        <label>
+                        <label htmlFor='player'>
                             {t('fullname')}
                         </label>
                         <input
@@ -47,39 +82,57 @@ export const RegisterPage = () => {
                             className={register.input_player}
                             name='player'
                             value={player}
-                            onChange={onInputChange}
-                            
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+
                         />
+                        {
+                           touched.player && errors.player && <span className={register.error}>{errors.player}</span>
+                        }
                     </div>
                     <div className={register.form_control}>
-                        <label>
+                        <label htmlFor='number'>
                             {t('number')}
-                            <strong>*</strong>
                         </label>
                         <input
                             type="text"
                             placeholder='Número del jugador'
-                            className={register.input_player}
+                            className={`${register.input_player}`}
                             name='number'
                             value={number}
-                            onChange={onInputChange}
-                            required
-                            
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            pattern='[0-9]*'
+                            maxLength={2}
+
                         />
+                        {
+                          touched.number && errors.number && <span className={register.error}>{errors.number}</span>
+                        }
 
                     </div>
                     <div className={register.form_control}>
-                        <label>
+                        <label htmlFor='position'>
                             {t('position')}
                         </label>
-                        <input
-                            type="text"
-                            placeholder='Posición del jugador'
+                        <select 
+                            name="position" 
+                            id="position_voley"
                             className={register.input_player}
-                            name='position'
                             value={position}
-                            onChange={onInputChange}
-                        />
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        >
+                            <option value="0">Seleccionar</option>
+                            <option value="3">{t('setter')}</option>
+                            <option value="1">{t('middle')}</option>
+                            <option value="2">{t('opposite')}</option>
+                            <option value="5">{t('outside')}</option>
+                            <option value="4">{t('libero')}</option>
+                        </select>
+                        {
+                           touched.position && errors.position && <span className={register.error}>{errors.position}</span>
+                        }
 
                     </div>
                     {/* <div className={register.form_control}>
